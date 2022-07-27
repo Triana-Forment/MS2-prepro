b43673dca79888372d15792e558b1f28cf391180
Triana-Forment <91782749+Triana-Forment@users.noreply.github.com>
2022-07-27 16:19:10 +0200

    Add files via upload

---
+++ run_several_MGFs_ionbot.py
@@ -0,0 +1,23 @@
+import glob
+import os
+
+# Create a list with MGF files
+files = [i for i in glob.glob("/public/compomics/triana/master/ionbot/PXD000561/*.mgf")]
+
+for file in files:
+
+        # Extension is the distinctive part from the file name (each sample)
+        extension = file.split("_")[5][0:3] 
+        # Directory is the working folder
+        directory = "/".join(file.split("/")[0:-1])
+        # Output path is directory + extension --> so ionbot output is in a unique folder for each sample
+        output_path = directory+"/"+extension+"/"
+        # Parts of the command to run Ionbot
+        part1 = "docker container run -v /public/compomics/triana/master/ionbot/PXD000561/:/public/compomics/triana/master/ionbot/PXD000561/ -w /public/compomics/triana/master/ionbot/PXD000561/ gcr.io/om>
+        part2 = " -a 32 -eRI --top-x-tags 30 "
+        comand  =  part1+output_path+part2+file
+        # Run the comand in the terminal
+        os.system(comand)
+
+
+
