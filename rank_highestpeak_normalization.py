13afe5a97872db20e2871e6638aafb6d7d70973e
Triana-Forment <91782749+Triana-Forment@users.noreply.github.com>
2022-08-08 14:55:24 +0200

    Add files via upload

---
+++ rank_highestpeak_normalization.py
@@ -0,0 +1,36 @@
+import sys
+import numpy as np
+from pyteomics import mgf
+
+def rank_TIC_normalization(input_file, output_file):
+
+    """
+    Normalizes spectra intensities taking into account their rank and the total ion current.
+    
+    Parameters
+    ----------
+        input_file : path to the mgf file to normalize
+        output_file : name of the mgf file normalized
+        
+    Returns
+    -------
+        Nothing, it writes an mgf file with the intensities normalized in the 'output_file'
+    """
+
+    reader = mgf.MGF(input_file)
+    for spectrum in reader:
+        spec_len = len(spectrum["intensity array"]) # Number of ions in MS2 spectrum
+        max_peak = np.max(spectrum["intensity array"]) # Highest peak intensity
+        original_order = spectrum["intensity array"].argsort().argsort() # get index to restore order
+        sorted_int = np.sort(spectrum["intensity array"])
+        normalized = np.zeros([spec_len,])
+        for index in range(spec_len):
+            normalized[index] = np.sum(sorted_int[:index+1])/max_peak
+        spectrum["intensity array"] = normalized[original_order]
+        mgf.write((spectrum,), output_file, file_mode='a')
+
+
+if __name__ == '__main__':
+    # Map command line arguments to function arguments.
+    rank_highestpeak_normalization(*sys.argv[1:])
+
